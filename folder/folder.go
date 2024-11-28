a983480d511871c8c0a90408973868e13d7a058e
Biswas Simkhada <biswas.simk@bissi-smells.modem>
2024-11-29 07:57:15 +1100

    sc take home init

--- folder/folder.go
+++ folder/folder.go
@@ -4,11 +4,11 @@ import "github.com/gofrs/uuid"
 
 type IDriver interface {
 	// GetFoldersByOrgID returns all folders that belong to a specific orgID.
-	GetFoldersByOrgID(orgID uuid.UUID) ([]Folder, error)
+	GetFoldersByOrgID(orgID uuid.UUID) []Folder
 	// component 1
 	// Implement the following methods:
 	// GetAllChildFolders returns all child folders of a specific folder.
-	GetAllChildFolders(orgID uuid.UUID, name string) ([]Folder, error)
+	GetAllChildFolders(orgID uuid.UUID, name string) []Folder
 
 	// component 2
 	// Implement the following methods:
@@ -16,29 +16,18 @@ type IDriver interface {
 	MoveFolder(name string, dst string) ([]Folder, error)
 }
 
-type FolderNode struct {
-	Folder   *Folder
-	Children []*FolderNode
-}
-
 type driver struct {
-	folderNames []string
-	nameToNode  map[string]*FolderNode
+	// define attributes here
+	// data structure to store folders
+	// or preprocessed data
+
+	// example: feel free to change the data structure, if slice is not what you want
+	folders []Folder
 }
 
-func NewDriver(folders []Folder) (IDriver, error) {
-	var folderNames []string
-	for _, folder := range folders {
-		folderNames = append(folderNames, folder.Name)
-	}
-
-	nameToNode, err := BuildFolderTree(folders, folderNames)
-	if err != nil {
-		return nil, err
-	}
-
+func NewDriver(folders []Folder) IDriver {
 	return &driver{
-		folderNames: folderNames,
-		nameToNode:  nameToNode,
-	}, nil
+		// initialize attributes here
+		folders: folders,
+	}
 }
--- folder/get_folder.go
+++ folder/get_folder.go
@@ -1,57 +1,27 @@
 package folder
 
-import (
-	"github.com/gofrs/uuid"
-)
+import "github.com/gofrs/uuid"
 
 func GetAllFolders() []Folder {
 	return GetSampleData()
 }
 
-func (d *driver) GetFoldersByOrgID(orgID uuid.UUID) ([]Folder, error) {
-	res := []Folder{}
-	for _, name := range d.folderNames {
-		node, ok := d.nameToNode[name]
-		if !ok {
-			return nil, ErrUnexpectedError
-		}
-
-		folder := node.Folder
-
-		if folder.OrgId == orgID {
-			res = append(res, *folder)
-		}
-	}
-
-	return res, nil
-}
-
-func (d *driver) GetAllChildFolders(orgID uuid.UUID, name string) ([]Folder, error) {
-	node, folderExists := d.nameToNode[name]
-
-	if !folderExists {
-		return nil, ErrFolderDoesNotExist
-	}
-
-	folder := node.Folder
-
-	if folder.OrgId != orgID {
-		return nil, ErrFolderDoesNotExistInOrg
-	}
+func (f *driver) GetFoldersByOrgID(orgID uuid.UUID) []Folder {
+	folders := f.folders
 
 	res := []Folder{}
-	getDescendants(&res, node, orgID)
+	for _, f := range folders {
+		if f.OrgId == orgID {
+			res = append(res, f)
+		}
+	}
 
-	return res, nil
+	return res
+
 }
 
-// adds all descendants of node to res
-func getDescendants(res *[]Folder, node *FolderNode, orgID uuid.UUID) {
-	for _, child := range node.Children {
-		if child.Folder.OrgId == orgID {
-			*res = append(*res, *child.Folder)
-		}
+func (f *driver) GetAllChildFolders(orgID uuid.UUID, name string) []Folder {
+	// Your code here...
 
-		getDescendants(res, child, orgID)
-	}
+	return []Folder{}
 }
